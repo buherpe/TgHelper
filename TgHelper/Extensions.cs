f7beea0a8392947833608020b2cb0c2841dbb41a
Anatoly Filyuk <buherpet.38@gmail.com>
2023-05-08 12:17:00 +0500

    добавлен метод расширения для уменьшения кол-ва кода

---
+++ TgHelper/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot;
+
+namespace TgHelper
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddTelegramBotClientWithWebhooks(this IServiceCollection services)
+        {
+            services.AddTransient<ITelegramBotClient>(sp =>
+            {
+                return new TelegramBotClient(sp.GetService<IConfiguration>()["TgBot:Token"]);
+            });
+
+            services.AddHostedService<ConfigureWebhook>();
+
+            return services;
+        }
+    }
+}
